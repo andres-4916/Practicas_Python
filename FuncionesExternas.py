a32c103c9fce66274174b0317e9b107a604a1614
Andrés Ramírez Olivares <60194030+andres-4916@users.noreply.github.com>
2021-06-01 17:20:21 -0500

    Add files via upload

---
+++ FuncionesExternas.py
@@ -0,0 +1,12 @@
+#Declaracion de funciones
+#Funcion sin parametros 
+
+def funcion1 ():
+#Contenido de funcion
+print ('Has llamado a la funcion 1')
+
+#Funcion con parametros y retorno
+def funcion2 (a,b):
+#Contenido de la funcion
+c=int (a) +int(b)
+return c
---
+++ Llamado de Funciones.py
@@ -0,0 +1,9 @@
+#Llamamos las funciones declaradas en otro archivo
+from FuncionesExternas import *
+
+#invocamos las funciones
+funcion1()
+#Invocamos la otra funcion
+Q=input ('introduzca un dato:')
+W=input ('Introduzca otro dato:')
+print(funcion2 (Q,W))
